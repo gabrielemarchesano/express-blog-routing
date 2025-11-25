1f2f42ab310262fca4facfa9e2f55f98e5b11f45
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-25 12:26:44 +0100

    add router for post to Read all the elements

--- routers/posts.js
+++ routers/posts.js
@@ -44,4 +44,13 @@ const posts = [
   },
 ];
 
+const express = require ("express");
+const router = express.Router();
+
+// index
+router.get("/", (req, res) => {
+  res.send("Lista dei post");
+});
+
+module.exports = router;
 module.exports = posts;
