6ae5ec2fc68b8bf8788751dcf8c83a96d21203d3
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-25 11:55:34 +0100

    define first route and start the server

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require ("express");
 const app = express();
 const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in the port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Blog routing");
+})
